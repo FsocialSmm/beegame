9e100ee0a4c0945253075a79518b8deb705c4083
root <root@v336786.hosted-by-vdsina.ru>
2020-09-11 22:02:26 +0300

    Updates

---
+++ precache-manifest.59dd48ef00f544b8cbcdcb505695137e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69b038f42f1c79ebf3eed5abbc9d0ad",
+    "url": "/beegame/index.html"
+  },
+  {
+    "revision": "2cac80be620f651916ad",
+    "url": "/beegame/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "f1691d293cd491a613ca",
+    "url": "/beegame/static/css/main.8ac4ba0a.chunk.css"
+  },
+  {
+    "revision": "2cac80be620f651916ad",
+    "url": "/beegame/static/js/2.932b4906.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/beegame/static/js/2.932b4906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1691d293cd491a613ca",
+    "url": "/beegame/static/js/main.4f7dfe8b.chunk.js"
+  },
+  {
+    "revision": "49ee29f86d3457536092",
+    "url": "/beegame/static/js/runtime-main.9a883c5f.js"
+  },
+  {
+    "revision": "5c10d8f77bacac993dbfe6e651f25557",
+    "url": "/beegame/static/media/logo.5c10d8f7.png"
+  }
+]);
